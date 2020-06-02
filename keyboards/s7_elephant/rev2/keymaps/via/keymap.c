f727ce776f065ea1d66e489ff65f85a669e5ff1a
MudkipMao <splatt9198@gmail.com>
2020-06-02 16:07:30 -0700

    Small changes to layouts, via firmware is too large q

--- keyboards/s7_elephant/rev2/keymaps/via/keymap.c
+++ keyboards/s7_elephant/rev2/keymaps/via/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------|   |-------------------------------------------------------------------------------------------------|
     * |  F4  |   | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |    RShift   |  U  | End |
     * |------|   |-------------------------------------------------------------------------------------------------|
-    * |  F5  |   | Ctrl |  Gui  |  Alt  |              Space                |   Ralt  |     Fn    |  L  | D  |  R  |
+    * |  F5  |   | Ctrl |  Gui  |  Alt  |              Space                | RAlt |  FN  | RCtrl |  L  | D  |  R  |
     * `------'   `-------------------------------------------------------------------------------------------------'
     */
     [_BASE] = LAYOUT_ansi(
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F2, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
         KC_F3, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      KC_ENT,      KC_HOME,
         KC_F4, KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-        KC_F5, KC_LCTL, KC_LGUI, KC_LALT,                      KC_SPC,                          KC_RALT, MO(1),            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_F5, KC_LCTL, KC_LGUI, KC_LALT,                      KC_SPC,                 KC_RALT, MO(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* Fn1 Layer 
     * ,------.   ,-------------------------------------------------------------------------------------------------.
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------|   |-------------------------------------------------------------------------------------------------|
     * |  F9  |   |           |Prev |Play |Next |     |     |     |     |     |     |     |             |     |PgDn |
     * |------|   |-------------------------------------------------------------------------------------------------|
-    * |  F10 |   |      |       |       |                                   |         |           |     |    |     |
+    * |  F10 |   |      |       |       |                                   |      |      |       |     |    |     |
     * `------'   `-------------------------------------------------------------------------------------------------'
     */
     [_FN1] = LAYOUT_ansi(
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F7,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_F8,   KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,       KC_PGUP,
         KC_F9,   KC_TRNS,          KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN,
-        KC_F10,  KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,                       KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_F10,  KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 
     /* Fn2 Layer 
     * ,------.   ,-------------------------------------------------------------------------------------------------.
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------|   |-------------------------------------------------------------------------------------------------|
     * |      |   |           |     |     |     |     |     |     |     |     |     |     |             |     |     |
     * |------|   |-------------------------------------------------------------------------------------------------|
-    * |      |   |      |       |       |                                   |         |           |     |    |     |
+    * |      |   |      |       |       |                                   |      |      |       |     |    |     |
     * `------'   `-------------------------------------------------------------------------------------------------'
     */
     [_FN2] = LAYOUT_ansi(
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,
         KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,                       KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 
     /* Fn3 Layer 
     * ,------.   ,-------------------------------------------------------------------------------------------------.
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------|   |-------------------------------------------------------------------------------------------------|
     * |      |   |           |     |     |     |     |     |     |     |     |     |     |             |     |     |
     * |------|   |-------------------------------------------------------------------------------------------------|
-    * |      |   |      |       |       |                                   |         |           |     |    |     |
+    * |      |   |      |       |       |                                   |      |      |       |     |    |     |
     * `------'   `-------------------------------------------------------------------------------------------------'
     */
     [_FN3] = LAYOUT_ansi(
@@ -88,5 +88,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,
         KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,                       KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 };
